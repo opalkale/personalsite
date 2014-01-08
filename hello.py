f75ca87fdba35fc4022db9adaa9620640ad0009f
Opal Kale <opal.kale@gmail.com>
2014-01-08 02:27:05 -0500

    init

--- hello.py
+++ hello.py
@@ -1,8 +1,16 @@
 import os
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+@app.route('/index')
+def index():
+    user = { 'nickname': 'Miguel' } # fake user
+    return render_template("index.html",
+        title = 'Home',
+        user = user)
+
+if __name__ == '__main__':
+    app.run(debug=True)
