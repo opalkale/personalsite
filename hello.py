6f8de2d97c38d1a01a79afd564a8db8afa7e1e53
Opal Kale <opal.kale@gmail.com>
2014-10-15 15:31:06 -0700

    hotfix

--- hello.py
+++ hello.py
@@ -8,9 +8,10 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
+    user = { 'nickname': 'Opal' } # fake user
     return render_template("index.html",
         title = 'Home',
-
+        user = user)
 if __name__ == '__main__':
     app.run(debug=True)
 
